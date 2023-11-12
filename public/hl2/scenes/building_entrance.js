44c160674d77ca721354fd494edbafa3f1d80f3d
matteokeole <matteo@keole.net>
2023-11-12 22:50:35 +0100

    test: building_entrance OK

--- public/hl2/scenes/building_entrance.js
+++ public/hl2/scenes/building_entrance.js
@@ -1,7 +1,5 @@
-import {BoxGeometry} from "../../../src/geometries/index.js";
 import {PointLight} from "../../../src/lights/index.js";
-import {TextureMaterial} from "../../../src/materials/index.js";
-import {Matrix3, Vector3} from "../../../src/math/index.js";
+import {Vector3} from "../../../src/math/index.js";
 import {ENTITY_HEIGHT_STAND} from "../index.js";
 import {Mesh} from "../Mesh.js";
 
@@ -19,25 +17,16 @@ export async function setup(renderer) {
 
 	const position = new Vector3(50, ENTITY_HEIGHT_STAND, 0);
 
-	renderer.player = new Mesh(
-		new BoxGeometry(new Vector3(16, 16, 16)),
-		new TextureMaterial({
-			textureMatrix: Matrix3.identity(),
-			texture: renderer._textures["metal/metalcombine001.jpg"],
-			normalMap: renderer._textures["metal/metalcombine001_normal.jpg"],
-		}),
-	);
-	renderer.player.setPosition(position.clone());
-	renderer.player.buildHitbox();
-
-	camera.distance = new Vector3(0, 0, 64);
 	camera.position = position.clone();
 	camera.target = position.clone();
+	// camera.setDistance(new Vector3(0, 0, -64));
 
 	const meshes = await (await fetch("public/hl2/scenes/building_entrance.json")).json();
 
-	for (let i = meshes.length - 1; i >= 0; i--) {
-		if (meshes[i].label == null) continue;
+	for (let i = 0, length = meshes.length; i < length; i++) {
+		if (meshes[i].label == null) {
+			continue;
+		}
 
 		scene.meshes.push(Mesh.fromJSON(meshes[i], renderer._textures));
 	}
